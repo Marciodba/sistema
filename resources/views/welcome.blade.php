9056cd91ed23944f450a27e05a9f39e271934eab
Marciodba <marciodba@outlook.com>
2023-11-22 19:38:30 -0300

    2 colunas na pagina principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,9 +50,10 @@
                
                
                 <div class="mt-16">
+                    <div class="grid grid-cols-2 md:grid-cols-2  gap-6 lg:gap-8">
                     @foreach ($produtoFornecedors as $produtoFornecedor)
                  
-                    <div class="grid grid-cols-1 md:grid-cols-1 gap-6 lg:gap-8">
+                        <div>
 
                         <a href=  {{ isset($produtoFornecedor->site) ? $produtoFornecedor->site : "https://www.magazinevoce.com.br/magazinegastandoeamando/" }}  class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
@@ -87,10 +88,11 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
                             </svg>
                         </a>
-                     
-                    
                     </div>
+                    
+                   
                     @endforeach
+                </div>
                 </div>
 
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">

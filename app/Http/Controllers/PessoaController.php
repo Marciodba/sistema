63b06257288dc6c4fecb2f9fa74be80ee4122ca7
Marciodba <marciodba@outlook.com>
2023-11-27 00:01:38 -0300

    ajuste controle de cliente na pagina principal

--- app/Http/Controllers/PessoaController.php
+++ app/Http/Controllers/PessoaController.php
@@ -4,11 +4,40 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Pessoa;
+use App\Models\ArqFisicoCli;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class PessoaController extends Controller
 {
+
+
+    public function index()
+    {
+
+
+        $clientes = Pessoa::where('ativo', true)->orderBy('dtatualizacao','DESC')->limit(500)->get();
+        $img_icones = [];
+      
+        if (!empty($cliente)) {
+
+
+            $icones = $clientes->pluck('apelido');
+    
+           
+            $arqFisicos = ArqFisicoCli::select(DB::RAW("encode(arqfoto, 'base64') as imagem,codnome"))->whereIn('codnome', $icones)
+            ->get();
+     
+            foreach ($arqFisicos as $arqFisico) {
+
+                $img_icones[$arqFisico->codnome] = $arqFisico->imagem;
+            }
+       
+        }
+  
+        return view('welcome', compact('clientes', 'img_icones'));
+    }
     public function cadastrarUsuario()
     {
         $pessoas = Pessoa::with(['usuario'])->get();
